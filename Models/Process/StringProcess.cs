a645ee3b7590bbab9913494f3d91f90d3ce9f72f
AnhHuy1456 <anhhuy8400@gmail.com>
2023-06-29 10:22:47 +0700

    HAH

--- Models/Process/StringProcess.cs
+++ Models/Process/StringProcess.cs
@@ -4,19 +4,18 @@ using System.Text.RegularExpressions;
 namespace Baithi_HAH.Models.StringProcess
 {
     public string StringProcess
-    {
+    
         public String AutoGenerateCode(string strInput)
         {
-            string strResult = "", numPart = "", strPart = "" ;
-            numPart = Regex.Match(strInput, @"\d+").Value;
-            strPart = Regex.Match(strInput, @"\D+").Value;
-            int intPart = (Convert.ToInt32(numPart) + 1);
-            for (int i = 0; i < numPart.Length - intPart.ToString().Length; i++)
+            string numPart="", strPart="", strResult="" ;
+            numPart = Regex.Match(Convert.ToString(@"\d+")).Value;
+            strPart = Regex.Match(Convert.ToString(@"\D+")).Value;
+            int intPart = (numPart) + 1 ;
+            for( int i=0; strPart.Length - (ToInt32.intPart).Length  ;i++)
             {
-                strPart += "0";
+                strResult +="0";
             }
-            strResult = strPart + intPart;
             return strResult;
         }
-    }
+    
 }
