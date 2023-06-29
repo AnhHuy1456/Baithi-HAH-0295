fe81516a65541bb0a26add3739417e3ef3106f1e
AnhHuy1456 <anhhuy8400@gmail.com>
2023-06-29 09:07:22 +0700

    HAHCau3

--- Models/HAHCau3.cs
+++ Models/HAHCau3.cs
@@ -6,8 +6,11 @@ namespace Baithi_HAH.Models
     public class HAHCau3
     {
         [Key]
+        [Display(Name ="Mã sinh viên")]
         public string Masinhvien { get; set;}
+        [Display(Name ="Họ tên sinh viên")]
         public string Hoten { get; set;}
+        [Display(Name ="Năm sinh sinh viên")]
         public string Namsinh { get; set;}
     }
 }
